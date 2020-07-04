09c3362d422e657745d1b9baa4c8a973c047f328
hizircanbayram <hizircanbayram@gmail.com>
2020-07-04 22:22:04 +0300

    accelerometer data sent to com6 with the rest

--- Core/Src/states/main_mode.c
+++ Core/Src/states/main_mode.c
@@ -35,6 +35,7 @@ void mainScreen(const CalorieInfo *person_cal_info) {
 	char text2[25] = { 0 };
 	sprintf(text2, "   ETA:  %ld:%ld:%ld", hour, min, sec);
 	ST7735_WriteString(0, 100, text2, TEXT_FONT_MAIN_MODE, TEXT_COLOR_MAIN_MODE, TEXT_BACKGROUND_COLOR_MAIN_MODE);
+	getAccData();
 }
 
 
@@ -47,8 +48,8 @@ void convertSecToTimeStamp(uint32_t elapsed_time, uint32_t* hour_ptr, uint32_t* 
 
 void getAccData() {
 	AccData acc_3d;
-	while (1)
-	{
+	//while (1)
+	//{
 		HAL_StatusTypeDef is_mma8452q_read_ok = mma8452qRead(&hi2c1, 0x00, 7, acc_3d.acc_info);
 		if (is_mma8452q_read_ok == HAL_OK) {
 			getAccXYZ(&acc_3d);
@@ -59,8 +60,9 @@ void getAccData() {
 			double z_acc = acc_3d.z_acc / 1024.;
 			//double magnitude = sqrt((x_acc * x_acc) + (y_acc * y_acc) + (z_acc * z_acc));
 			sprintf(message, " %.2f %.2f %.2f", x_acc, y_acc, z_acc);
-			ST7735_WriteString(0, 50, message, TEXT_FONT_MAIN_MODE, TEXT_COLOR_MAIN_MODE, TEXT_BACKGROUND_COLOR_MAIN_MODE);
-			/*HAL_StatusTypeDef blue_ok = HAL_UART_Transmit(&huart6, (uint8_t *)message, sizeof(message), 100);
+			//ST7735_WriteString(0, 50, message, TEXT_FONT_MAIN_MODE, TEXT_COLOR_MAIN_MODE, TEXT_BACKGROUND_COLOR_MAIN_MODE);
+			HAL_StatusTypeDef blue_ok = HAL_UART_Transmit(&huart6, (uint8_t *)message, sizeof(message), 100);
+			/*
 			if (blue_ok == HAL_OK) {
 				ST7735_WriteString(0, 100, "ok...", Font_7x10, ST7735_BLACK, ST7735_WHITE);
 			}
@@ -72,5 +74,5 @@ void getAccData() {
 		else {
 			// uart ile buraya mesaj bas
 		}
-	}
+	//}
 }
