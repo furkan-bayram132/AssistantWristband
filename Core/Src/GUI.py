6650f03f0002ea663ace03b5b0a6dea0ba9903e4
hizircanbayram <hizircanbayram@gmail.com>
2020-07-07 05:32:55 +0300

    3-axis accelerometer datas monitored in real time

--- Core/Src/GUI.py
+++ Core/Src/GUI.py
@@ -8,12 +8,13 @@ import random
 # seed random number generator
 seed(1)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
+fig, axs = plt.subplots(3)
+fig.suptitle('Accelerometer Datas')
+#ax = fig.add_subplot(111)
 fig.show()
 plt.ylim([-1,1])
 i = 0
-x, y = [], []
+x, acc_x, acc_y, acc_z = [], [], [], []
 
 ser = serial.Serial('COM6', baudrate=9600)
 if(ser.isOpen() == False):
@@ -21,15 +22,20 @@ if(ser.isOpen() == False):
 
 while True:
     datas = ser.readline().decode("utf-8").split(' ')
-    print(float(datas[1]))
+    #print(float(datas[1]))
     x.append(i)
-    y.append(float(datas[1]))
-    
-    ax.plot(x, y, color='b')
+    acc_x.append(float(datas[1]))
+    acc_y.append(float(datas[2]))
+    acc_z.append(float(datas[3]))    
+    axs[0].plot(x, acc_x, color='b')
+    axs[1].plot(x, acc_y, color='b')
+    axs[2].plot(x, acc_z, color='b')
     
     fig.canvas.draw()
     
-    ax.set_xlim(left=max(0, i-50), right=i+50)
+    axs[0].set_xlim(left=max(0, i-50), right=i+50)
+    axs[1].set_xlim(left=max(0, i-50), right=i+50)
+    axs[2].set_xlim(left=max(0, i-50), right=i+50)
     
     time.sleep(0.1)
     i += 1
